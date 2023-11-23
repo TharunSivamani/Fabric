a622bafc569ab83adb9c5e799f04b1caf6813195
TharunSivamani <tharunsivamani@gmail.com>
2023-11-23 11:13:43 +0530

    mnist-vanilla

--- Examples/Image Classifier/train_vanilla.py
+++ Examples/Image Classifier/train_vanilla.py
@@ -92,6 +92,9 @@ def run(hparams):
                     )
                 )
 
+                if hparams.dry_run:
+                    break
+            
             scheduler.step()
 
             # Testing Loop
@@ -107,6 +110,9 @@ def run(hparams):
                     test_loss += F.nll_loss(output, target, reduction="sum").item()
                     pred = output.argmax(dim=1, keepdim=True)
                     correct += pred.eq(target.view_as(pred)).sum().item()
+
+                    if hparams.dry_run:
+                        break
             
             test_loss /= len(test_loader.dataset)
 
@@ -116,10 +122,31 @@ def run(hparams):
             )
         )
 
+        if hparams.dry_run:
+            break
+
+    if hparams.save_model:
+        torch.save(model.state_dict(), "mnist_cnn.pt")
+
 
 def main():
     parser = argparse.ArgumentParser(description="PyTorch MNIST Example")
-    
+    parser.add_argument(
+        "--batch-size", type=int, default=64, metavar="N", help="input batch size for training (default: 64)"
+    )
+    parser.add_argument("--epochs", type=int, default=14, metavar="N", help="number of epochs to train (default: 14)")
+    parser.add_argument("--lr", type=float, default=1.0, metavar="LR", help="learning rate (default: 1.0)")
+    parser.add_argument("--gamma", type=float, default=0.7, metavar="M", help="Learning rate step gamma (default: 0.7)")
+    parser.add_argument("--dry-run", action="store_true", default=False, help="quickly check a single pass")
+    parser.add_argument("--seed", type=int, default=1, metavar="S", help="random seed (default: 1)")
+    parser.add_argument(
+        "--log-interval",
+        type=int,
+        default=10,
+        metavar="N",
+        help="how many batches to wait before logging training status",
+    )
+    parser.add_argument("--save-model", action="store_true", default=False, help="For Saving the current Model")
     hparams = parser.parse_args()
     run(hparams)
 
